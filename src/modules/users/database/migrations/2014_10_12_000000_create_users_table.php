f00b7b76f050f4c753457509668cd1b3533d57b6
Federico Francescato <federico@p3in.com>
2015-09-23 09:37:14 -0400

    added migration for profile_users to create_users_table

--- src/modules/users/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/modules/users/database/migrations/2014_10_12_000000_create_users_table.php
@@ -47,6 +47,24 @@ class CreateUsersTable extends Migration
 		});
 	}
 
+	//
+	//
+	Schema::create('profile_user', function (Blueprint $table) {
+		$table->increments('id');
+		$table->integer('user_id')->unsigned();
+		$table->foreign('user_id')
+			->references('id')
+			->on('users')
+			->onDelete('cascade');
+
+		$table->integer('profilable_id')->unsigned();
+		$table->integer('profilable_type');
+
+	});
+
+	// profilable_id = 1
+	// profilable_type = \BostonPads\Models\LandlrdProfile
+
 	/**
 	 * Reverse the migrations.
 	 *
@@ -54,11 +72,12 @@ class CreateUsersTable extends Migration
 	 */
 	public function down()
 	{
-		Schema::table('permission_user', function( $table ) {
-			$table->dropForeign('permission_user_permission_id_foreign');
-			$table->dropForeign('permission_user_user_id_foreign');
-		});
+		// Schema::table('permission_user', function( $table ) {
+		// 	$table->dropForeign('permission_user_permission_id_foreign');
+		// 	$table->dropForeign('permission_user_user_id_foreign');
+		// });
 		Schema::drop('permission_user');
+		Schema::drop('profile_users');
 		Schema::drop('users');
 	}
 }
