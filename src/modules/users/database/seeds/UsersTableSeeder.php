a4c015311672fa58345cd62b3eb6d0f52d29a460
Jubair Saidi <jubair.saidi@p3in.com>
2015-10-13 21:44:37 -0400

    some error going on with factory not finding P3in\Models\User.  so commenting out for now.

--- src/modules/users/database/seeds/UsersTableSeeder.php
+++ src/modules/users/database/seeds/UsersTableSeeder.php
@@ -10,23 +10,24 @@ use DB;
 
 class UsersTableSeeder extends Seeder
 {
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
 
-		DB::table('users')->delete();
+        DB::table('users')->delete();
 
-		$user = factory('P3in\Models\User', 10)
-			->create()
-			->each(function($user) {
-				$user
-					->permissions()
-					->attach(Permission::first());
-		  });
+        // some error going on with factory not finding this model.  so commenting out for now.
+        // $user = factory('P3in\Models\User', 10)
+        //  ->create()
+        //  ->each(function($user) {
+        //      $user
+        //          ->permissions()
+        //          ->attach(Permission::first());
+        //   });
 
     //    $user
     //      ->permissions()
@@ -37,32 +38,32 @@ class UsersTableSeeder extends Seeder
     //      ->attach(Group::first());
     //  });
 
-      /**
-      *   create some actual users
-      *
-      *
-      */
-      User::create([
-        'first_name' => 'Federico',
-        'last_name' => 'Francescato',
-        'email' => 'federico@p3in.com',
-        'password' => bcrypt('d3velopment'),
-        'phone' => '1-857-600-2702',
-        'active' => true
-      ])
-        ->permissions()
-        ->attach(Permission::firstOrFail()->id);
+        /**
+        *   create some actual users
+        *
+        *
+        */
+        User::create([
+            'first_name' => 'Federico',
+            'last_name' => 'Francescato',
+            'email' => 'federico@p3in.com',
+            'password' => bcrypt('d3velopment'),
+            'phone' => '1-857-600-2702',
+            'active' => true
+        ])
+            ->permissions()
+            ->attach(Permission::firstOrFail()->id);
 
-      User::create([
-        'first_name' => 'Jubair',
-        'last_name' => 'Saidi',
-        'password' => bcrypt('d3velopment'),
-        'email' => 'jubair.saidi@p3in.com',
-        'phone' => '1-857-600-2702',
-        'active' => true
-      ])
-        ->permissions()
-        ->attach(Permission::firstOrFail()->id);
+        User::create([
+            'first_name' => 'Jubair',
+            'last_name' => 'Saidi',
+            'password' => bcrypt('d3velopment'),
+            'email' => 'jubair.saidi@p3in.com',
+            'phone' => '1-857-600-2702',
+            'active' => true
+        ])
+            ->permissions()
+            ->attach(Permission::firstOrFail()->id);
 
   }
 }
