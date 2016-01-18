e9b2b5ecea870fc1ad54674cf8dad11cbeab19d2
Federico Francescato <federico@p3in.com>
2016-01-18 16:59:46 -0500

    added form-submission route

--- src/modules/pages/Controllers/PagesController.php
+++ src/modules/pages/Controllers/PagesController.php
@@ -10,6 +10,7 @@ use Illuminate\View\Factory;
 use P3in\Models\Page;
 use P3in\Models\Section;
 use P3in\Models\Website;
+use Mail;
 
 class PagesController extends Controller
 {
@@ -49,6 +50,35 @@ class PagesController extends Controller
         $data['navmenus'] = json_decode(json_encode($data['navmenus']));
 
         return view('layouts.master.'.str_replace(':', '_', $page->layout), $data);
+
+    }
+
+    public function submitForm(Request $request)
+    {
+
+        $website = Website::current();
+
+        $from = $website->config->from_email ?: 'info@bostonpads.com';
+
+        $to = $request->get('to') ?: $website->config->default_recipients;
+
+        $data = $request->except(['_token', 'to', 'heading', 'subheading', 'text', 'style']);
+
+        Mail::send('mail.form-submission', ['website' => $website, 'data' => $data], function($message) use($from, $to, $request) {
+            $message->from($from)
+                ->to($to)
+                ->subject('Form submission');
+
+                foreach($request->file() as $field_name => $file) {
+
+                    $message->attach($file->getRealPath(), [
+                        'as' => $file->getClientOriginalExtension(),
+                        'mime' => $file->getMimeType()
+                    ]);
+
+                }
+
+            });
 
     }
 }
