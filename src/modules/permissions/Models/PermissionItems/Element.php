87273ed1d008ed0eafe85eb289fbcd1b00bb83e7
Federico Francescato <federico@p3in.com>
2016-02-04 13:38:48 -0500

    Added mandatory `how` method to Element

--- src/modules/permissions/Models/PermissionItems/Element.php
+++ src/modules/permissions/Models/PermissionItems/Element.php
@@ -2,10 +2,14 @@
 
 namespace P3in\Models\PermissionsRequired\PermissionItems;
 
+use Illuminate\Database\Eloquent\Builder;
 use P3in\Interfaces\PermissionRequiredItemInterface;
 
 class Element implements PermissionRequiredItemInterface {
 
-
+    public function how(Builder $query)
+    {
+        return $query;
+    }
 
 }
