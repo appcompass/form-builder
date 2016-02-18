f1db732f46d2f9c474f87bf0fbc763e635f6dacb
Federico Francescato <federico@p3in.com>
2016-02-18 16:11:15 -0500

    Better flow, better frontend logic for groups/permissions assignment 'owned' property is being filled on-the-fly

--- src/modules/permissions/Views/assign.blade.php
+++ src/modules/permissions/Views/assign.blade.php
@@ -12,7 +12,7 @@
                 <tr>
                     <th>Label</th>
                     <th>Description</th>
-                    <th>Permissions</th>
+                    <th v-if="owned[0] && owned[0].permissions">Permissions</th>
                     <th></th>
                 </tr>
             </thead>
@@ -21,17 +21,15 @@
                 <tr v-for="single in owned">
                     <td>@{{ single.label }}</td>
                     <td>@{{ single.description }}</td>
-                    <td>
-                        <div v-if="single.permissions">
-                            <ul>
-                                <li v-for="perm in single.permissions">@{{ perm.label }}</li>
-                            </ul>
-                        </div>
+                    <td v-if="single.permissions">
+                        <ul>
+                            <li v-for="perm in single.permissions">@{{ perm.label }}</li>
+                        </ul>
                     </td>
                     <td>
                         <a
                             href
-                            v-on:click.prevent="remove(single)"
+                            v-on:click="remove(single)"
                             class="btn btn-xs btn-danger"
                         ><i class="fa fa-trash-o"> </i></a>
                     </td>
@@ -49,13 +47,16 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="single in avail">
+                <tr
+                    v-for="single in avail"
+                    v-bind:class="{disabled: single.isOwned}"
+                >
                     <td>@{{ single.label }}</td>
-                    <td>@{{ single.description }}</td>
+                    <td>@{{ single.description }} @{{ single.isOwned }}</td>
                     <td>
                         <a
                             href
-                            v-on:click.prevenr="add(single)"
+                            v-on:click="add(single)"
                             class="btn btn-xs btn-success"
                         >
                             <i class="fa fa-plus"></i>
@@ -67,12 +68,22 @@
         </table>
 
         <div class="tools pull-right">
-            <a class="btn btn-primary" v-on:click.prevent="store()">Save</a>
+            <a
+                href="#"
+                class="btn btn-primary"
+                v-on:click.prevent="store()"
+            >Save</a>
         </div>
     </div>
 @stop
 
 @section('footer.scripts')
+
+<style>
+    .disabled {
+        opacity: 0.5;
+    }
+</style>
 
 <script>
     (function(Vue) {
@@ -83,49 +94,82 @@
             owner: {!! json_encode($owner) !!}
         };
 
+        /// DATA INIT
+        data.avail.forEach(function(item) {
+            item.isOwned = false;
+        })
+
         var Vue = new Vue({
             el: '#permissions-manager',
-            data: data,
-
-            ready: function() {
-                this.resource = this.$resource("{{ $meta->base_url }}");
+            data: {
+                avail: data.avail,
+                owned: new Array()
             },
-
             methods: {
-                add: function(item) {
+                add: add,
+                remove: remove,
+                store: store,
+            },
+            ready: function() {
+                var self = this;
+                self.resource = self.$resource("{{ $meta->base_url }}");
+                self.avail.forEach(function(item) {
+                    if (has(item, self.owned)) {
+                        item.isOwned = true;
+                    }
+                })
 
-                    var found = this.owned.some(function(el) {
+                data.owned.forEach(function(item) {
+
+                    // var item = self.avail.some(function(el) {
+                    //     return el.id === item.id;
+                    // })
+
+                    var item = self.avail.find(function(el) {
                         return el.id === item.id;
                     })
 
-                    if (!found) {
-                        this.owned.push(item);
-                    }
+                    self.add(item);
+                })
+            },
 
-                },
+        });
 
-                remove: function(item) {
-                    this.owned.$remove(item);
-                },
+        function add(item) {
 
-                store: function() {
-                    this.resource.save({
-                        owned: this.owned
-                    }).then(function(response) {
-                    }, function(error) {
-                        console.error(error);
-                    })
-                },
-
-                http: {
-                    root: '/root',
-                    headers: {
-                       'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                }
+            if (!has(item, this.owned)) {
+                item.isOwned = true;
+                return this.owned.push(item);
             }
-        })
 
+        }
+
+        function remove(item) {
+            item.isOwned = false;
+            return this.owned.$remove(item);
+        }
+
+        function store() {
+            this.resource.save({
+                owned: this.owned
+            }).then(function(response) {
+                return true;
+            }, function(error) {
+                console.error(error);
+            })
+        }
+
+        ///// PRIVATE
+
+        function has(needle, hay, property) {
+
+            property = undefined !== property ? property: 'id';
+
+            return hay.some(function(el) {
+                return el[property] === needle[property];
+            })
+
+        }
     })(Vue)
 </script>
 @stop
