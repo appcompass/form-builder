5f7eefecf696a62da9f1f035dbeab0ae591c0296
Jubair Saidi <jubair.saidi@p3in.com>
2016-02-23 16:35:06 -0500

    cleaned up users module updating indentation and adding proper profile support via polymorphic relationship.

--- src/modules/users/Models/User.php
+++ src/modules/users/Models/User.php
@@ -24,42 +24,42 @@ use P3in\Traits\OptionableTrait;
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
 
-	use Authenticatable, CanResetPassword, Alertable, Authorizable, OptionableTrait;
+    use Authenticatable, CanResetPassword, Alertable, Authorizable, OptionableTrait;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = [
-		'first_name',
-		'last_name',
-		'phone',
-		'email',
-		'password',
-		'active'
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+        'password',
+        'active'
+    ];
 
-	/**
-	 * 	The attributes excluded from the model's JSON form.
-	 *
-	 * 	@var array
-	 */
-	protected $hidden = ['password', 'remember_token'];
+    /**
+     *  The attributes excluded from the model's JSON form.
+     *
+     *  @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
 
-	/**
-	 * Stuff to append to each request
-	 *
-	 *
-	 */
-	protected $appends = ['full_name'];
+    /**
+     * Stuff to append to each request
+     *
+     *
+     */
+    protected $appends = ['full_name'];
 
     public static $validator_rules = [
         'first_name' => 'required|max:255',
@@ -69,212 +69,200 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         'password' => 'required|max:255',
     ];
 
-	/**
-	*	Get all the permissions the user has
-	*
-	*
-	*/
-	public function permissions()
-	{
+    /**
+    *   Get all the permissions the user has
+    *
+    *
+    */
+    public function permissions()
+    {
 
-		return $this->belongsToMany(Permission::class)->withTimestamps();
+        return $this->belongsToMany(Permission::class)->withTimestamps();
 
-	}
+    }
 
-	/**
-	 * Return all the permissions of the user
-	 *
-	 * @return array permission owned by the user
-	 */
-	public function allPermissions()
-	{
+    /**
+     * Return all the permissions of the user
+     *
+     * @return array permission owned by the user
+     */
+    public function allPermissions()
+    {
 
-		// @TODO  add getCacheKey or something, the id is overly non-specific. should return class_name . id . updated_at_timestamp
+        // @TODO  add getCacheKey or something, the id is overly non-specific. should return class_name . id . updated_at_timestamp
 
-		return Cache::remember($this->id, 1, function() {
+        return Cache::remember($this->id, 1, function() {
 
-			// $this->load(['groups.permissions' => function($query) { $query->where('active', true); }])
-			$this->load('groups.permissions')
-				->load('permissions');
+            // $this->load(['groups.permissions' => function($query) { $query->where('active', true); }])
+            $this->load('groups.permissions')
+                ->load('permissions');
 
-			$perms = collect($this->permissions->lists('type', 'id'));
+            $perms = collect($this->permissions->lists('type', 'id'));
 
-			$this->groups
-				->each(function($group) use($perms) {
-					$group->permissions
-						->lists('type', 'id')
-						->each(function($perm, $key) use($perms) {
-							$perms->push($perm);
-						});
-			});
+            $this->groups
+                ->each(function($group) use($perms) {
+                    $group->permissions
+                        ->lists('type', 'id')
+                        ->each(function($perm, $key) use($perms) {
+                            $perms->push($perm);
+                        });
+            });
 
-			return $perms->unique();
+            return $perms->unique();
 
-		});
+        });
 
-	}
+    }
 
-	/**
-	 * Adds a permission to the user
-	 */
-	public function grantPermission(Permission $perm)
-	{
-		return $this->permissions()->attach($perm);
-	}
+    /**
+     * Adds a permission to the user
+     */
+    public function grantPermission(Permission $perm)
+    {
+        return $this->permissions()->attach($perm);
+    }
 
-	/**
-	 * Revokes a user's permission
-	 */
-	public function revokePermission(Permission $perm)
-	{
-	  	return $this->permissions()->detach($perm);
-	}
+    /**
+     * Revokes a user's permission
+     */
+    public function revokePermission(Permission $perm)
+    {
+        return $this->permissions()->detach($perm);
+    }
 
-	/**
-	 *	Check if user has a single permission
-	 *
-	 *	@param string $permission Permission type.
-	 *	@return bool
-	 */
-	public function hasPermission($permission)
-	{
+    /**
+     *  Check if user has a single permission
+     *
+     *  @param string $permission Permission type.
+     *  @return bool
+     */
+    public function hasPermission($permission)
+    {
 
-		if (is_array($permission)) {
+        if (is_array($permission)) {
 
-			return $this->hasPermissions($permission);
+            return $this->hasPermissions($permission);
 
-		}
+        }
 
-		return in_array($permission, $this->allPermissions()->toArray());
+        return in_array($permission, $this->allPermissions()->toArray());
 
-	}
+    }
 
-	/**
-	 *	Check if user has a group of permissions
-	 *
-	 *	@param array permissions
-	 *	@return bool
-	 */
-	public function hasPermissions($permissions)
-	{
+    /**
+     *  Check if user has a group of permissions
+     *
+     *  @param array permissions
+     *  @return bool
+     */
+    public function hasPermissions($permissions)
+    {
 
-		$permissions = explode(",", $permissions);
+        $permissions = explode(",", $permissions);
 
-		if (count($permissions) == 0) {
-			return true;
-		}
+        if (count($permissions) == 0) {
+            return true;
+        }
 
-		return (bool)count(array_intersect($this->allPermissions()->toArray(), $permissions)) == count($permissions);
+        return (bool)count(array_intersect($this->allPermissions()->toArray(), $permissions)) == count($permissions);
 
-	}
+    }
 
-	/**
-	 *  Get all the groups this user belongs to
-	 *
-	 *
-	 */
-	public function groups()
-	{
+    /**
+     *  Get all the groups this user belongs to
+     *
+     *
+     */
+    public function groups()
+    {
 
-		return $this->belongsToMany(Group::class)->withTimestamps();
+        return $this->belongsToMany(Group::class)->withTimestamps();
 
-	}
+    }
 
-	/**
-	  * Add current user to a group
-	  */
-	public function addToGroup(Group $group)
-	{
-	  	return $group->addUser($this);
-	}
+    /**
+      * Add current user to a group
+      */
+    public function addToGroup(Group $group)
+    {
+        return $group->addUser($this);
+    }
 
-	/**
-	  *  Remove current user from a group
-	  */
-	public function removeFromGroup(Group $group)
-	{
-	  	return $group->removeUser($this);
-	}
+    /**
+      *  Remove current user from a group
+      */
+    public function removeFromGroup(Group $group)
+    {
+        return $group->removeUser($this);
+    }
 
-	/**
-	 *	Get either all or a specific profile type of a user
-	 *
-	 *
-	 *	TODO: this needs to be refactored a little
-	 */
-	public function profiles($type = null)
-	{
+    /**
+     *  Get either all or a specific profile type of a user
+     *
+     *
+     *  TODO: this needs to be refactored a little
+     */
+    public function profiles()
+    {
+        return $this->hasMany(BaseProfile::class);
+    }
 
-	  $relation = $this->hasMany(BaseProfile::class);
-
-	  if (! is_null($type)) {
-
-	  	$profile_class = $relation->where('model', $type)->firstOrFail();
-
-	  	return new $profile_class->model($profile_class->toArray());
-
-	  }
-
-	  return $relation;
-
-	}
-
-	public function photos()
-	{
-	  return $this->hasMany(Photo::class);
-	}
-	public function galleries()
-	{
-	  return $this->hasMany(Gallery::class);
-	}
+    public function photos()
+    {
+      return $this->hasMany(Photo::class);
+    }
+    public function galleries()
+    {
+      return $this->hasMany(Gallery::class);
+    }
 
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = bcrypt($value);
     }
 
-	/**
-	 *	Get user's full name
-	 *
-	 */
-	public function getFullNameAttribute()
-	{
+    /**
+     *  Get user's full name
+     *
+     */
+    public function getFullNameAttribute()
+    {
 
-		return sprintf("%s %s", $this->first_name, $this->last_name);
+        return sprintf("%s %s", $this->first_name, $this->last_name);
 
-	}
+    }
 
-	/**
-	 *	Get/Set user's Avatar
-	 *
-	 *
-	 */
-	public function avatar(Photo $photo = null)
-	{
+    /**
+     *  Get/Set user's Avatar
+     *
+     *
+     */
+    public function avatar(Photo $photo = null)
+    {
 
-		if (! Modular::isDef('photos')) {
+        if (! Modular::isDef('photos')) {
 
-			$userEmail = \Auth::user()->email;
-			return "//www.gravatar.com/avatar/".md5($userEmail)."?s={$size}";
+            $userEmail = \Auth::user()->email;
+            return "//www.gravatar.com/avatar/".md5($userEmail)."?s={$size}";
 
-		}
+        }
 
-		if (! is_null($photo)) {
+        if (! is_null($photo)) {
 
-			if (! is_null($this->avatar)) {
+            if (! is_null($this->avatar)) {
 
-				$this->avatar()
-					->first()
-					->unlink();
+                $this->avatar()
+                    ->first()
+                    ->unlink();
 
-			}
+            }
 
-			$this->avatar()->save($photo);
+            $this->avatar()->save($photo);
 
-		}
+        }
 
-		return $this->morphOne(Photo::class, 'photoable');
+        return $this->morphOne(Photo::class, 'photoable');
 
-	}
+    }
 
 }
--- src/modules/users/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/modules/users/database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,72 +5,72 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function (Blueprint $table) {
-			$table->increments('id');
-			$table->string('first_name');
-			$table->string('last_name');
-			$table->string('phone', 24);
-			$table->string('email')->unique();
-			$table->string('password', 64);
-			$table->boolean('active')->default(false);
-			$table->string('activation_code', 64)->nullable();
-			$table->timestamp('activated_at', 64)->nullable();
-			$table->timestamp('last_login', 64)->nullable();
-			$table->rememberToken();
-			$table->timestamps();
-		});
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone', 24);
+            $table->string('email')->unique();
+            $table->string('password', 64);
+            $table->boolean('active')->default(false);
+            $table->string('activation_code', 64)->nullable();
+            $table->timestamp('activated_at', 64)->nullable();
+            $table->timestamp('last_login', 64)->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
 
-		# link user to his profiles
-		// Schema::create('profile_user', function (Blueprint $table) {
-		// 	$table->increments('id');
+        # link user to his profiles
+        // Schema::create('profile_user', function (Blueprint $table) {
+        //  $table->increments('id');
 
-		// 	$table->integer('user_id')->unsigned();
-		// 	$table->foreign('user_id')
-		// 		->references('id')
-		// 		->on('users')
-		// 		->onDelete('cascade');
+        //  $table->integer('user_id')->unsigned();
+        //  $table->foreign('user_id')
+        //      ->references('id')
+        //      ->on('users')
+        //      ->onDelete('cascade');
 
-		// 	$table->integer('profilable_id')->unsigned();
-		// 	$table->integer('profilable_type');
-		// });
+        //  $table->integer('profilable_id')->unsigned();
+        //  $table->integer('profilable_type');
+        // });
 
-		# link permissions to user
-		Schema::create('permission_user', function(Blueprint $table) {
-			$table->integer('permission_id')->unsigned()->index();
-			$table->foreign('permission_id')
-				    ->references('id')
-				    ->on('permissions')
-				    ->onDelete('cascade');
+        # link permissions to user
+        Schema::create('permission_user', function(Blueprint $table) {
+            $table->integer('permission_id')->unsigned()->index();
+            $table->foreign('permission_id')
+                    ->references('id')
+                    ->on('permissions')
+                    ->onDelete('cascade');
 
-			$table->integer('user_id')->unsigned()->index();
-			$table->foreign('user_id')
-				    ->references('id')
-				    ->on('users')
-				    ->onDelete('cascade');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
 
-			// $table->unique(['permission_id', 'user_id']);
-			$table->primary(['permission_id', 'user_id']);
-			$table->timestamps();
-		});
+            // $table->unique(['permission_id', 'user_id']);
+            $table->primary(['permission_id', 'user_id']);
+            $table->timestamps();
+        });
 
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('permission_user');
-		Schema::drop('profile_users');
-		Schema::drop('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('permission_user');
+        Schema::drop('profile_users');
+        Schema::drop('users');
+    }
 }
