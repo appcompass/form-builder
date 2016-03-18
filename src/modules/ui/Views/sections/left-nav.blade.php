100af8ec811b65c6299a1c80a75497c5d3212d65
Federico Francescato <federico@p3in.com>
2016-03-18 10:14:58 -0400

    Left nav items are collapsable using jQuery. Working on the Vue app.

--- src/modules/ui/Views/sections/left-nav.blade.php
+++ src/modules/ui/Views/sections/left-nav.blade.php
@@ -2,12 +2,12 @@
 <div class="leftside-navigation">
     <ul class="sidebar-menu" id="nav-accordion">
         @foreach($nav as $item)
-            <li @if(!empty($item->children)) class="sub-menu" @endif >
+            <li class="collapsable" @if(!empty($item->children)) sub-menu @endif">
                 <a {!! inlineAttrs($item->props, 'link') !!} >
                     <i class="fa fa-{{ $item->props->icon or 'list' }}"></i> {{ $item->label }}
                 </a>
                 @if(!empty($item->children))
-                    <ul class="sub">
+                    <ul class="sub" v-bind:class="{'toggled': isHidden}">
 
                         @foreach($item->children as $sub_item)
                             <li>
@@ -23,29 +23,13 @@
             </li>
         @endforeach
 
-{{--     @foreach($nav->navitems as $item)
-        <li @if($nav->hasChildren($item->linked_id)) class="sub-menu" @endif>
-            <a href="{{ $item->url }}" {!! inlineAttrs($item->props, 'link') !!}>
-                <i class="fa fa-{{ $item->navitem->props['icon'] or 'list' }}"></i>{{ $item->label }}
-            </a>
-
-            @if ($nav->hasChildren($item->linked_id))
-                <ul class="sub">
-
-                    @foreach($nav->children->find($item->linked_id)->navitems as $sub_item)
-                        <li>
-                            <a href="{{ $sub_item->url }}" {!! inlineAttrs($sub_item->navitem->props, 'link') !!}>
-                                <i class="fa fa-{{ $sub_item->navitem->props['icon'] or "list" }}"> </i>
-                                {{ $sub_item->label }}
-                            </a>
-                        </li>
-                    @endforeach
-
-                </ul>
-            @endif
-
-        </li>
-    @endforeach
- --}}
     </ul>
 </div>
+
+<script>
+    (function() {
+        $('.collapsable').on('click', function() {
+            $(this).find('ul').first().toggleClass('hidden')
+        })
+    })()
+</script>
