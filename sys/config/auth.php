f8d3a38461067d3a5af0f0b57acd630c4c21ad02
Jubair Saidi <jubair.saidi@p3in.com>
2016-04-12 16:57:53 -0400

    updated default user of app to the cms core user model.

--- sys/config/auth.php
+++ sys/config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => P3in\Models\User::class,
         ],
 
         // 'users' => [
