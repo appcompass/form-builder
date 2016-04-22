973a7203418e78671c3f2e9130c774a660ea6cfd
Jubair Saidi <jubair.saidi@p3in.com>
2016-04-22 09:06:36 -0400

    fixed galleries controller to use the new init method.

--- src/modules/galleries/Controllers/CpGalleriesController.php
+++ src/modules/galleries/Controllers/CpGalleriesController.php
@@ -8,35 +8,14 @@ use P3in\Models\Gallery;
 
 class CpGalleriesController extends UiBaseResourceController
 {
-    public function __construct(Request $request, Gallery $galleries)
+    public function __construct(Request $request)
     {
-        $this->init($request, $galleries);
+        $this->init($request, function($routes){
+            if (!empty($routes->params['galleries'])) {
+                return $routes->params['galleries'];
+            }else{
+                return new Gallery;
+            }
+        });
     }
-
-    // /**
-    //  *
-    //  *
-    //  */
-    // public function store(Request $request)
-    // {
-    //     $gallery = new Gallery($request->all());
-
-    //     $this->record = $this->user->galleries()->save($gallery);
-
-    //     return $this->json($this->setBaseUrl(['galleries', $this->record->id, 'photos']));
-
-    // }
-
-    // /**
-    //  *
-    //  *
-    //  */
-    // public function update(Request $request, Gallery $galleries)
-    // {
-    //     $this->record = $galleries;
-
-    //     $this->record->update($request->all());
-
-    //     return $this->json($this->setBaseUrl(['galleries', $galleries->id, 'edit']));
-    // }
 }
