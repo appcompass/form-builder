810c019425ce49993df04b98e8c1f739ec09c69c
Michael Farrell <msfarrel@gmail.com>
2016-06-10 13:04:19 -0400

    Home page frontend

--- source/assets/js/src/main.js
+++ source/assets/js/src/main.js
@@ -1,6 +1,6 @@
 (function($) {
 
-	//var breakpoints = {large: 1200, medium: 992, small:768 , xsmall:767 };
+	var breakpoints = {large: 1200, medium: 992, small:768 , xsmall:767 };
 
 
 	/* Navigation
@@ -15,5 +15,71 @@
 		e.preventDefault();
 		$(this).parent().parent().toggleClass('is-open');
 	});
+
+	/* Slideshows
+	---------------------------------------------------------------------- */
+	$('.work-slideshow').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		arrows: false,
+		mobileFirst: true,
+		customPaging : function(slider, i) {
+			var num = i + 1;
+			return '<button type="button" data-role="none" role="button" tabindex="0">0'+ num + '</button>';
+		},
+	});
+
+	$('.facebook-slideshow').slick({
+		centerMode: true,
+  		centerPadding: '20px',
+  		infinite:false,
+		speed: 300,
+		arrows: false,
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: breakpoints.xsmall,
+				settings: {
+					variableWidth: true,
+					centerMode: false,
+				}
+			},
+			{
+				breakpoint: breakpoints.medium - 1,
+				settings: {
+					variableWidth: true,
+					centerMode: false,
+					arrows: true,
+					appendArrows: '.facebook-slideshow-controls',
+					prevArrow: '<button type="button" class="slick-prev"><span class="icon-arrow"></span></button>',
+					nextArrow: '<button type="button" class="slick-next"><span class="icon-arrow"></span></button>',
+				}
+			}
+
+		]
+	});
+
+	$('.review-slideshow').slick({
+		fade:true,
+		autoplay:true,
+		autoplaySpeed: 4000,
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		slidesToShow: 1,
+		arrows: false	
+	});
+
+	/* Match Heights
+	---------------------------------------------------------------------- */
+	$('.home-solution-box').matchHeight();
+	$('.home-solution-front').matchHeight();
+	$('.facebook-slide').matchHeight();
+
+	if ($(window).width() < breakpoints.small) {
+		$('.home-solution-box, .home-solution-front, .facebook-slide').matchHeight({ remove: true });
+	}
 
 })(jQuery);
--- source/home.php
+++ source/home.php
@@ -2,6 +2,341 @@
 
 <?php include('header.php'); ?>
 
-asdf
+<section class="home-banner">
+	<div class="home-banner-header"> 
+		<div class="home-banner-header-container">
+			<h1 class="home-banner-heading">Our Work</h1>
+		</div>	
+	</div>
+	<div class="work-slideshow">
+		<article class="work-slide">
+			<div class="work-slide-container">
+				<div class="work-slide-image"><img src="assets/images/content/slide_atmgurus.png"></div>
+				<div class="work-slide-content">
+					<h2 class="work-slide-title">ATM Gurus</h2>
+					<div class="work-slide-desc">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis tortor lobortis, ultrices risus ac.</p>
+					</div>
+					<a href="" class="work-slide-btn"><span class="icon-plus"></span> See More</a>
+				</div>	
+			</div>
+		</article>
+		<article class="work-slide">
+			<div class="work-slide-container">
+				<div class="work-slide-image"><img src="assets/images/content/slide_atmgurus.png"></div>
+				<div class="work-slide-content">
+					<h2 class="work-slide-title">ATM Gurus</h2>
+					<div class="work-slide-desc">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis tortor lobortis, ultrices risus ac.</p>
+					</div>
+					<a href="" class="work-slide-btn"><span class="icon-plus"></span> See More</a>
+				</div>	
+			</div>
+		</article>
+	</div>
+</section><!-- home-banner -->
+
+<section class="home-section home-solutions">
+	<div class="row">
+		<div class="medium-8 medium-centered columns">
+			<h2 class="home-section-heading home-solutions-heading">Targeted Web Solutions</h2>
+			<div class="home-section-desc home-solutions-desc">
+				<p>We develop custom web applications that meet the unique needs of your business. Plus 3 Interactive can help your company meet its strategic goals:</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="medium-6 columns">
+			<div class="row">
+				<div class="small-6 columns">
+					<a href="" class="home-solution-box">
+						<span class="home-solution-front">
+							<span class="home-solution-icon"><img src="assets/images/content/solution_web_apps.png"></span>
+							<span class="home-solution-title">Custom Web Applications</span>
+						</span>
+						<span class="home-solution-back">
+							<span class="home-solution-back-center">
+								<span class="home-solution-title">Custom Web Applications</span>
+								<ul class="home-solution-list">
+									<li>Middleware designed for your requirements</li>
+									<li>Integrated with legacy &amp; proprietary systems</li>
+									<li>Web 2.0 standards</li>
+								</ul>
+								<span class="home-solution-more"><span class="icon-plus"></span> See More</span>
+							</span>
+						</span>
+					</a>
+				</div>
+				<div class="small-6 columns">
+					<a href="" class="home-solution-box">
+						<span class="home-solution-front">
+							<span class="home-solution-icon"><img src="assets/images/content/solution_middleware.png"></span>
+							<span class="home-solution-title">Middleware Development</span>
+						</span>
+						<span class="home-solution-back">
+							<span class="home-solution-back-center">
+								<span class="home-solution-title">Custom Web Applications</span>
+								<ul class="home-solution-list">
+									<li>Middleware designed for your requirements</li>
+									<li>Integrated with legacy &amp; proprietary systems</li>
+									<li>Web 2.0 standards</li>
+								</ul>
+								<span class="home-solution-more"><span class="icon-plus"></span> See More</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="medium-6 columns">
+			<div class="row">
+				<div class="small-6 columns">
+					<a href="" class="home-solution-box">
+						<span class="home-solution-front">
+							<span class="home-solution-icon"><img src="assets/images/content/solution_device_comm.png"></span>
+							<span class="home-solution-title">Device Web Communications</span>
+						</span>
+						<span class="home-solution-back">
+							<span class="home-solution-back-center">
+								<span class="home-solution-title">Custom Web Applications</span>
+								<ul class="home-solution-list">
+									<li>Middleware designed for your requirements</li>
+									<li>Integrated with legacy &amp; proprietary systems</li>
+									<li>Web 2.0 standards</li>
+								</ul>
+								<span class="home-solution-more"><span class="icon-plus"></span> See More</span>
+							</span>
+						</span>
+					</a>
+				</div>
+				<div class="small-6 columns">
+					<a href="" class="home-solution-box">
+						<span class="home-solution-front">
+							<span class="home-solution-icon"><img src="assets/images/content/solution_app_management.png"></span>
+							<span class="home-solution-title">Web Application Management Systems</span>
+						</span>
+						<span class="home-solution-back">
+							<span class="home-solution-back-center">
+								<span class="home-solution-title">Custom Web Applications</span>
+								<ul class="home-solution-list">
+									<li>Middleware designed for your requirements</li>
+									<li>Integrated with legacy &amp; proprietary systems</li>
+									<li>Web 2.0 standards</li>
+									<li>Web 2.0 standards</li>
+								</ul>
+								<span class="home-solution-more"><span class="icon-plus"></span> See More</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section><!-- home-solutions -->
+
+<section class="home-section home-process">
+	<div class="row">
+		<div class="medium-8 medium-centered columns">
+			<h2 class="home-section-heading home-process-heading">Our Process</h2>
+			<div class="home-section-desc home-process-desc">
+				<p>Explore our development process …</p>
+			</div>
+		</div>
+	</div>
+</section><!-- home-process -->
+
+<section class="home-section home-team">
+	<div class="row">
+		<div class="medium-8 medium-centered columns">
+			<h2 class="home-section-heading home-team-heading">The Team</h2>
+			<div class="home-section-desc home-team-desc">
+				<p>Plus 3 Interactive is a customer-focused business. Our team finds the right solutions to our customers’ unique challenges.</p>
+				<p><a href=""><strong>Check out the people who make our company work</strong> <span class="icon-arrow"></span></a>
+			</div>
+		</div>
+	</div>
+	<div class="home-team-members">
+		<a href="" class="home-team-member" style="background-image:url('assets/images/content/team_imene.jpg');">
+			<span class="team-hover">
+				<span class="team-name">Imene Saidi</span>
+				<span class="team-position">Information Architect</span>
+			</span>
+		</a>
+		<a href="" class="home-team-member" style="background-image:url('assets/images/content/team_jubair.jpg');">
+			<span class="team-hover">
+				<span class="team-name">Jubair Saidi</span>
+				<span class="team-position">Information Architect</span>
+			</span>
+		</a>
+		<a href="" class="home-team-member" style="background-image:url('assets/images/content/team_aisha.jpg');">
+			<span class="team-hover">
+				<span class="team-name">Aisha Saidi</span>
+				<span class="team-position">Information Architect</span>
+			</span>
+		</a>
+		<a href="" class="home-team-member" style="background-image:url('assets/images/content/team_lazarus.jpg');">
+			<span class="team-hover">
+				<span class="team-name">Lazarus Morrison</span>
+				<span class="team-position">Information Architect</span>
+			</span>
+		</a>
+	</div>
+</section><!-- home-team -->
+
+<section class="home-section home-social">
+	<div class="row">
+		<div class="xsmall-12 columns">
+			<div class="home-social-heading">
+				<h2 class="home-section-heading">Plus 3 Interactive <span class="color-blue">-</span> Active!</h2>
+				<div class="home-social-follow">
+					Follow Us <a class="icon-instagram"></a> <a class="icon-twitter"></a> <a class="icon-facebook"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="facebook-slideshow-wrap">
+		
+		<div class="facebook-slideshow">
+			<div class="facebook-slide">
+				<div class="fb-header">
+					<div class="fb-avatar"><img src="assets/images/content/fb_avatar.png"></div>
+					<h3 class="fb-user"><a href="">Plus 3 Interactive</a></h3>
+					<div class="fb-followers">106 followers</div>
+					<a href="" class="fb-icon icon-facebook-square"></a>
+				</div>
+				<div class="fb-post">
+					<span class="fb-post-img"><img src="assets/images/content/fb_post_img.jpg"></span>
+					<h2 class="fb-post-title">P3 at the Boston Dragon Boat Festival 2015</h2>
+					<p class="fb-post-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque fringilla condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
+				</div>
+				<div class="fb-footer">
+					<a>Like</a>
+					<a>Comment</a>
+					<a>Share</a>
+					<span class="fb-likes">30 people like this</span>
+				</div>
+			</div>
+
+			<div class="facebook-slide">
+				<div class="fb-header">
+					<div class="fb-avatar"><img src="assets/images/content/fb_avatar.png"></div>
+					<h3 class="fb-user"><a href="">Plus 3 Interactive</a></h3>
+					<div class="fb-followers">106 followers</div>
+					<a href="" class="fb-icon icon-facebook-square"></a>
+				</div>
+				<div class="fb-post">
+					<span class="fb-post-img"><img src="assets/images/content/fb_post_img.jpg"></span>
+					<h2 class="fb-post-title">P3 at the Boston Dragon Boat Festival 2015</h2>
+					<p class="fb-post-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque fringilla condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
+				</div>
+				<div class="fb-footer">
+					<a>Like</a>
+					<a>Comment</a>
+					<a>Share</a>
+					<span class="fb-likes">30 people like this</span>
+				</div>
+			</div>
+
+			<div class="facebook-slide">
+				<div class="fb-header">
+					<div class="fb-avatar"><img src="assets/images/content/fb_avatar.png"></div>
+					<h3 class="fb-user"><a href="">Plus 3 Interactive</a></h3>
+					<div class="fb-followers">106 followers</div>
+					<a href="" class="fb-icon icon-facebook-square"></a>
+				</div>
+				<div class="fb-post">
+					<span class="fb-post-img"><img src="assets/images/content/fb_post_img.jpg"></span>
+					<h2 class="fb-post-title">P3 at the Boston Dragon Boat Festival 2015</h2>
+					<p class="fb-post-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque fringilla condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
+				</div>
+				<div class="fb-footer">
+					<a>Like</a>
+					<a>Comment</a>
+					<a>Share</a>
+					<span class="fb-likes">30 people like this</span>
+				</div>
+			</div>
+
+			<div class="facebook-slide">
+				<div class="fb-header">
+					<div class="fb-avatar"><img src="assets/images/content/fb_avatar.png"></div>
+					<h3 class="fb-user"><a href="">Plus 3 Interactive</a></h3>
+					<div class="fb-followers">106 followers</div>
+					<a href="" class="fb-icon icon-facebook-square"></a>
+				</div>
+				<div class="fb-post">
+					<span class="fb-post-img"><img src="assets/images/content/fb_post_img.jpg"></span>
+					<h2 class="fb-post-title">P3 at the Boston Dragon Boat Festival 2015</h2>
+					<p class="fb-post-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque fringilla condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
+				</div>
+				<div class="fb-footer">
+					<a>Like</a>
+					<a>Comment</a>
+					<a>Share</a>
+					<span class="fb-likes">30 people like this</span>
+				</div>
+			</div>
+
+			<div class="facebook-slide">
+				<div class="fb-header">
+					<div class="fb-avatar"><img src="assets/images/content/fb_avatar.png"></div>
+					<h3 class="fb-user"><a href="">Plus 3 Interactive</a></h3>
+					<div class="fb-followers">106 followers</div>
+					<a href="" class="fb-icon icon-facebook-square"></a>
+				</div>
+				<div class="fb-post">
+					<span class="fb-post-img"><img src="assets/images/content/fb_post_img.jpg"></span>
+					<h2 class="fb-post-title">P3 at the Boston Dragon Boat Festival 2015</h2>
+					<p class="fb-post-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque fringilla condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
+				</div>
+				<div class="fb-footer">
+					<a>Like</a>
+					<a>Comment</a>
+					<a>Share</a>
+					<span class="fb-likes">30 people like this</span>
+				</div>
+			</div>
+
+		</div><!-- facebook-slideshow -->
+		<div class="facebook-slideshow-controls"></div>
+			
+	</div><!-- facebook-slideshow-wrap -->
+</section><!-- home-social -->
+
+<section class="home-section home-reviews">
+	<div class="row">
+		<div class="xsmall-12 columns">
+			<div class="review-slideshow">
+				<div class="review-slide">
+					<blockquote class="review">
+						<span class="icon-quote open-quote"></span>
+						Plus 3 Interactive helped us build out 2 new sites for our most popular brands, integrated our existing database, and seamlessly migrated over 45,000 products to the new sites.
+						<cite>- Chris Vallely, Christian Brands</cite>
+						<span class="icon-quote close-quote"></span>
+					</blockquote>
+				</div>
+
+				<div class="review-slide">
+					<blockquote class="review">
+						<span class="icon-quote open-quote"></span>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget elementum arcu. Donec viverra porttitor blandit. Cras maximus tortor vel tristique fermentum.
+						<cite>- Chris Vallely, Christian Brands</cite>
+						<span class="icon-quote close-quote"></span>
+					</blockquote>
+				</div>
+
+				<div class="review-slide">
+					<blockquote class="review">
+						<span class="icon-quote open-quote"></span>
+						Cras eu eleifend nibh. Etiam bibendum porta arcu quis venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						<cite>- Chris Vallely, Christian Brands</cite>
+						<span class="icon-quote close-quote"></span>
+					</blockquote>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php include('footer.php'); ?>	
