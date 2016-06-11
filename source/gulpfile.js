22ea8dca03f17ae069b7b4fecf3cd6d7ceafcb06
Jubair Saidi <jubair.saidi@p3in.com>
2016-06-11 00:39:52 -0400

    removed .DS_Store files from repo and updated gitignore to properly ignore all instances of .DS_Store.  updated gulpfile.js to compile front-end build to public folder.

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var del = require('del');
+var rename = require("gulp-rename");
 var sass = require('gulp-sass');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
 var gulpIf = require('gulp-if');
 var runSequence = require('run-sequence');
@@ -12,31 +13,41 @@ gulp.task('clean:dist', function() {
   return del.sync(['../www/**', '!../www', '!../www/index.php'], {force: true});
 });
 
-gulp.task('sass', function(){
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('../www/css'));
-});
-
-gulp.task('useref', function(){
-    return gulp.src('src/templates/**/*.html')
-        .pipe(useref())
-        .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulpIf('*.css', cssnano()))
+gulp.task('templates', function(){
+    return gulp.src('*.php')
         .pipe(gulp.dest('../www'));
 });
 
+gulp.task('sass', function(){
+    return gulp.src('assets/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(rename('main-min.css'))
+        .pipe(gulp.dest('../www/assets/css'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        'assets/bower_components/slick-carousel/slick/slick.min.js',
+        'assets/bower_components/matchHeight/jquery.matchHeight-min.js',
+        'assets/js/src/*.js'
+    ])
+        .pipe(uglify())
+        .pipe(concat('main-min.js'))
+        .pipe(gulp.dest('../www/assets/js/dist'));
+});
+
 gulp.task('images', function(){
-    return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    return gulp.src('assets/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('../www/images'));
+        .pipe(gulp.dest('../www/assets/images'));
 });
 
 gulp.task('fonts', function() {
-    return gulp.src('src/fonts/**/*')
-        .pipe(gulp.dest('../www/fonts'));
+    return gulp.src('assets/fonts/**/*')
+        .pipe(gulp.dest('../www/assets/fonts'));
 });
 
 gulp.task('build', function(callback) {
-    runSequence('clean:dist', ['sass', 'useref', 'images', 'fonts'], callback);
+    runSequence('clean:dist', ['sass', 'scripts', 'images', 'fonts', 'templates'], callback);
 });
