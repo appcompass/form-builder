e21e33f8a244585537b60de5fec99ddf8505f3b9
Michael Farrell <msfarrel@gmail.com>
2016-06-24 17:13:57 -0400

    project template - incomplete

--- source/projects.php
+++ source/projects.php
@@ -15,6 +15,83 @@
 	</div>
 </section><!-- banner -->
 
+<section class="section-project" style="background-image:url('assets/images/content/bg_project_atmgurus.jpg');">
+	<div class="section-project-content">
+		<div class="row">
+			<div class="small-6 columns">
+				<div class="project-logo"><img src="assets/images/content/logo_atmgurus.svg" width="457" height="164"></div>
+				<div class="project-services">
+					<p><strong>Plus 3 Interactive Services:</strong></p>
+					<ul>
+						<li>Custom Web application</li>
+						<li>Application management system (AMS)</li>
+						<li>Middleware development</li>
+					</ul>
+				</div>
+				<a href="" class="btn-white btn-project">View Details</a>
+			</div>
+		</div>
+	</div>
+</section><!-- section-project -->
+
+<section class="section-project" style="background-image:url('assets/images/content/bg_project_bostonpads.jpg');">
+	<div class="section-project-content">
+		<div class="row">
+			<div class="small-6 columns">
+				<div class="project-logo"><img src="assets/images/content/logo_bostonpads.svg" width="532" height="56"></div>
+				<div class="project-services">
+					<p><strong>Plus 3 Interactive Services:</strong></p>
+					<ul>
+						<li>Custom Web application</li>
+						<li>Application management system (AMS)</li>
+						<li>Middleware development</li>
+					</ul>
+				</div>
+				<a href="" class="btn-white btn-project">View Details</a>
+			</div>
+		</div>
+	</div>
+</section><!-- section-project -->
+
+<section class="section-project" style="background-image:url('assets/images/content/bg_project_versalink.jpg');">
+	<div class="section-project-content">
+		<div class="row">
+			<div class="small-6 columns">
+				<div class="project-logo"><img src="assets/images/content/logo_versalink.svg" width="393" height="167"></div>
+				<div class="project-services">
+					<p><strong>Plus 3 Interactive Services:</strong></p>
+					<ul>
+						<li>Custom Web application</li>
+						<li>Application management system (AMS)</li>
+						<li>Middleware development</li>
+					</ul>
+				</div>
+				<a href="" class="btn-white btn-project">View Details</a>
+			</div>
+		</div>
+	</div>
+</section><!-- section-project -->
+
+<section class="section-project" style="background-image:url('assets/images/content/bg_project_pronto.jpg');">
+	<div class="section-project-content">
+		<div class="row">
+			<div class="small-6 columns">
+				<div class="project-logo"><img src="assets/images/content/logo_pronto.png" width="249" height="204"></div>
+				<div class="project-services">
+					<p><strong>Plus 3 Interactive Services:</strong></p>
+					<ul>
+						<li>Custom Web application</li>
+						<li>Application management system (AMS)</li>
+						<li>Middleware development</li>
+					</ul>
+				</div>
+				<a href="" class="btn-white btn-project">View Details</a>
+			</div>
+		</div>
+	</div>
+</section><!-- section-project -->
+
+
 
 
 <?php include('footer.php'); ?>
