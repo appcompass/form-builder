576be409ef86005302de391735541552a5605e99
Michael Farrell <msfarrel@gmail.com>
2016-06-30 20:20:52 -0400

    process v1

---
+++ source/process.php
@@ -0,0 +1,72 @@
+<?php $title = "Mock Ups"; $page_class = ""; ?>
+
+<?php include('header.php'); ?>
+
+<div class="banner" style="background-image:url('');">
+	<div class="banner-content">
+		<div class="row">
+			<div class="medium-8 medium-centered columns"> 
+				<h1 class="banner-title">Our Process</h1>
+			</div>
+		</div>
+	</div>
+</div><!-- banner -->
+
+<div class="section-breadcrumb">
+	<div class="row">
+		<div class="small-6 columns">
+			<p class="breadcrumb"><a href="">Solutions</a> <span class="sep">/</span> Our Process</p>
+		</div>
+		<div class="small-6 columns small-text-right">
+			<a href="">Learn more about Plus 3 Interactive <span class="icon-arrow"></span></a>
+		</div>
+	</div>
+</div><!-- section-breadcrumb -->
+
+<div class="section-process">
+	<div class="row">
+		<div class="medium-8 medium-centered columns">
+			<h1>Steps to Software Success</h1>
+			<p>From our first meeting with you, through each stage of product development, we follow a clear process to deliver quality and keep you informed.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="xsmall-12 columns">
+			<div class="process-timeline">
+				<div class="process-step process-step-1">
+					<div class="process-step-top">
+						<div class="process-step-line"></div>
+						<div class="process-step-content">
+							<img class="process-step-icon" src="assets/images/content/icon_discovery.svg" width="86" height="140">
+							<h2 class="process-step-title">1 <span>Discovery</span></h2>
+						</div>
+					</div>
+					<div class="process-step-desc process-step-content">
+						<p>You tell us about your project and we develop a proposal of responsibilities, budget, and timeline.</p>
+					</div>
+				</div><!-- process-step-1 -->
+				<div class="process-step process-step-2">
+					<div class="process-step-top">
+						<div class="process-step-line"></div>
+						<div class="process-step-content">
+							<img class="process-step-icon" src="assets/images/content/icon_discovery.svg" width="86" height="140">
+							<h2 class="process-step-title">1 <span>Discovery</span></h2>
+						</div>
+					</div>
+					<div class="process-step-desc process-step-content">
+						<p>You tell us about your project and we develop a proposal of responsibilities, budget, and timeline.</p>
+					</div>
+				</div><!-- process-step-2 -->
+			</div><!-- process-timeline -->
+		</div>
+	</div>
+</div>
+
+<a href="" class="banner-cta">
+	<span class="banner-cta-content">
+		<h2 class="banner-cta-heading">Interested in what we can do for you? Please submit an RFP <span class="icon-arrow"></span></h2>
+	</span>
+</a>
+
+
+<?php include('footer.php'); ?>
