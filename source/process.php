d358a255e85261bab5c91a9599e3d32dafed6a44
Michael Farrell <msfarrel@gmail.com>
2016-07-01 12:55:32 -0400

    process template

--- source/process.php
+++ source/process.php
@@ -183,11 +183,26 @@
 </div>
 
 
-<a href="" class="banner-cta">
-	<span class="banner-cta-content">
-		<h2 class="banner-cta-heading">Interested in what we can do for you? Please submit an RFP <span class="icon-arrow"></span></h2>
-	</span>
-</a>
+<section class="banner-blue">
+	<div class="row">
+		<div class="xsmall-12 columns">
+			<div class="banner-blue-container">
+				<div class="banner-blue-icon"><img src="assets/images/content/icon_maintenance.svg" width="152" height="152"></div>
+				<div class="banner-blue-content">
+					<h2 class="banner-blue-heading">What about Maintenance?</h2>
+					<p>Many companies take a phased approach to roll out. After launch of your Web application, we can customize a maintenance plan that could include:</p>
+					<ul>
+						<li>Ongoing application design or development</li>
+						<li>Ongoing content development</li>
+						<li>Research and planning consultations</li>
+					</ul>
+					<p><b>Please ask us about our maintenance plans.</b></p>
+					<a href="" class="btn-white">Contact Us <span class="icon-arrow"></span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 
 <?php include('footer.php'); ?>
