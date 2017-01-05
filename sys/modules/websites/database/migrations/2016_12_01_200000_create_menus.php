95f17ce574dc46df169ecea98ff856e36545c386
Jubair Saidi <jubair.saidi@p3in.com>
2017-01-05 08:54:49 -0500

    moved table columns around in pages and menus table migrations so reading relationships on records is easier.

--- sys/modules/websites/database/migrations/2016_12_01_200000_create_menus.php
+++ sys/modules/websites/database/migrations/2016_12_01_200000_create_menus.php
@@ -15,11 +15,12 @@ class CreateMenus extends Migration
     {
         Schema::create('menus', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
 
             $table->integer('website_id')->unsigned();
             $table->foreign('website_id')->references('id')->on('websites');
+
+            $table->string('name');
+            $table->timestamps();
 
             $table->unique(['name', 'website_id']); // just for sanity check
             $table->index('name');
