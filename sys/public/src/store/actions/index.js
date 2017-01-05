6c6d54099a1b8d4dfb03ee92ad4f3bf949ccb92f
Jubair Saidi <jubair.saidi@p3in.com>
2017-01-04 19:05:38 -0500

    moved public folder contents into cp where it belongs, updated index.php file to correct version, changed websites module seeder to seed env file website name and url definitions.

--- sys/public/src/store/actions/index.js
+++
@@ -1,3 +0,0 @@
-export const increment = ({ commit }) => commit('increment')
-export const decrement = ({ commit }) => commit('decrement')
-export const getFucked = ({ commit, state }) => console.log('Get Fucked!')
