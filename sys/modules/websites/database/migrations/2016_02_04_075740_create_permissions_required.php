005f31a34140f5d30fec02ea1d901c63fd65a2d8
Federico <federico@p3in.com>
2017-01-10 08:43:15 -0500

    Comments out  creation in Websites module, Form->resource doesn't need to be unique (multiple forms can point at the same resource, it's context dependent), Shifts menu resource to the correct endpoint

--- sys/modules/websites/database/migrations/2016_02_04_075740_create_permissions_required.php
+++ sys/modules/websites/database/migrations/2016_02_04_075740_create_permissions_required.php
@@ -12,26 +12,26 @@ class CreatePermissionsRequired extends Migration
      */
     public function up()
     {
-        Schema::create('permissions_required', function(Blueprint $table) {
-            $table->increments('id');
+        // Schema::create('permissions_required', function(Blueprint $table) {
+        //     $table->increments('id');
 
-            $table->integer('permission_id')->unsigned();
-            $table->integer('website_id')->unsigned();
-            $table->string('type'); // ['route', 'element', 'controller']
-            $table->string('pointer');
-            $table->timestamps();
+        //     $table->integer('permission_id')->unsigned();
+        //     $table->integer('website_id')->unsigned();
+        //     $table->string('type'); // ['route', 'element', 'controller']
+        //     $table->string('pointer');
+        //     $table->timestamps();
 
-            $table->foreign('permission_id')
-                ->references('id')
-                ->on('permissions')
-                ->onDelete('cascade');
+        //     $table->foreign('permission_id')
+        //         ->references('id')
+        //         ->on('permissions')
+        //         ->onDelete('cascade');
 
-            $table->foreign('website_id')
-                ->references('id')
-                ->on('websites')
-                ->onDelete('cascade');
+        //     $table->foreign('website_id')
+        //         ->references('id')
+        //         ->on('websites')
+        //         ->onDelete('cascade');
 
-        });
+        // });
     }
 
     /**
@@ -41,6 +41,6 @@ class CreatePermissionsRequired extends Migration
      */
     public function down()
     {
-        Schema::drop('permissions_required');
+        // Schema::drop('permissions_required');
     }
 }
