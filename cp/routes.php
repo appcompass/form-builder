73fd05b4d4e230dc18f503865a4c6d364ed45a0f
Jubair Saidi <jubair.saidi@p3in.com>
2017-01-11 13:01:59 -0500

    synced hot fix changes to repo.

--- cp/routes.php
+++ cp/routes.php
@@ -7,19 +7,8 @@ Route::group([
 ], function($router) {
     // $router->get('notification-center', 'CpController@getNotificationCenter');
     // $router->get('dashboard', 'CpController@getDashboard');
+});
 
-
-    // // T@ODO: some day, it would be nice to allow this route to be the
-    // // index of any UI that uses this API where it takes into account
-    // // the authorized client's permissions on each route.
-    // $router->get('', function () use ($router) {
-    //     $rtn = [];
-    //     foreach ($router->getRoutes() as $route) {
-    //         $rtn[] = [
-    //             'methods' => $route->getMethods(),
-    //             'path' => $route->getPath(),
-    //         ];
-    //     }
-    //     return $rtn;
-    // });
+Route::get('', function () {
+    return '';
 });
