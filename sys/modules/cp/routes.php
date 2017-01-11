2eae243067f41767aef1910a38327b42307bd36b
Jubair Saidi <jubair.saidi@p3in.com>
2017-01-11 11:51:12 -0500

    removed route list from api index since it's now a public domain.

--- sys/modules/cp/routes.php
+++ sys/modules/cp/routes.php
@@ -9,17 +9,17 @@ Route::group([
     // $router->get('dashboard', 'CpController@getDashboard');
 
 
-    // T@ODO: some day, it would be nice to allow this route to be the
-    // index of any UI that uses this API where it takes into account
-    // the authorized client's permissions on each route.
-    $router->get('', function () use ($router) {
-        $rtn = [];
-        foreach ($router->getRoutes() as $route) {
-            $rtn[] = [
-                'methods' => $route->getMethods(),
-                'path' => $route->getPath(),
-            ];
-        }
-        return $rtn;
-    });
+    // // T@ODO: some day, it would be nice to allow this route to be the
+    // // index of any UI that uses this API where it takes into account
+    // // the authorized client's permissions on each route.
+    // $router->get('', function () use ($router) {
+    //     $rtn = [];
+    //     foreach ($router->getRoutes() as $route) {
+    //         $rtn[] = [
+    //             'methods' => $route->getMethods(),
+    //             'path' => $route->getPath(),
+    //         ];
+    //     }
+    //     return $rtn;
+    // });
 });
