d874bd9ce0bcf0dcd3f6e369cee93b94952fa437
Federico <federico@p3in.com>
2017-01-18 12:10:17 -0500

    Small UI tweak, Fixes Webpack compiling issue

--- cp/src/components/Login.vue
+++ cp/src/components/Login.vue
@@ -1,16 +1,17 @@
 <template lang="jade">
-div
-  p.is-danger(v-if="error") Login Error, fucko!
-  p.control.has-icon
-    input.input(type="email", placeholder="Email", v-model="email")
-    span.icon.is-small
-      i.fa.fa-envelope
-  p.control.has-icon
-    input.input(type="password", placeholder="Password", v-model="password")
-    span.icon.is-small
-      i.fa.fa-lock
-  p.control
-    button.button.is-success(@click="login") Login
+.columns
+  .column.is-half.is-offset-one-quarter
+    p.is-danger(v-if="error") Login Error, fucko!
+    p.control.has-icon
+      input.input(type="email", placeholder="Email", v-model="email")
+      span.icon.is-small
+        i.fa.fa-envelope
+    p.control.has-icon
+      input.input(type="password", placeholder="Password", v-model="password")
+      span.icon.is-small
+        i.fa.fa-lock
+    p.control
+      button.button.is-success(@click="login") Login
 </template>
 
 <script>
