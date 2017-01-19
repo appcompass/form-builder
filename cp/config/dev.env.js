52fc1caff3df96802c2d971589c369fe8b721ee2
Federico <federico@p3in.com>
2017-01-19 06:24:46 -0500

    dev conf merges from prod, we only specify the overrides

--- cp/config/dev.env.js
+++ cp/config/dev.env.js
@@ -2,6 +2,5 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_SERVER: '"https://api.p3in.com.dev/"'
+  NODE_ENV: '"development"'
 })
