d7b169787494683a094209a5f439eb6c55f6f0e9
Jubair Saidi <jubair.saidi@p3in.com>
2017-01-22 16:25:23 -0500

    Renamed FieldBuilder Component back to Fieldtype so it doesn't colide with the websites Components (which though are both components , are of very different types and purposes).  added (temp) fix to BaseField to allow plus3websites seeder to work, didn't have time to track down the root cause.

--- cp/src/components/Components.js
+++ cp/src/components/Components.js
@@ -2,7 +2,21 @@ import StringType from './FormBuilder/String'
 import TextType from './FormBuilder/Text'
 import SecretType from './FormBuilder/Secret'
 import MenuEditorType from './FormBuilder/MenuEditor'
+import FieldsetType from './FormBuilder/Fieldset'
+import WysiwygType from './FormBuilder/Wysiwyg'
+import FileType from './FormBuilder/File'
+import LinkType from './FormBuilder/Link'
+import FormBuilderType from './FormBuilder/FormBuilder'
+import MapType from './FormBuilder/Map'
+import LoginFormType from './FormBuilder/LoginForm'
 export var String = StringType
 export var Text = TextType
 export var Secret = SecretType
 export var MenuEditor = MenuEditorType
+export var Fieldset = FieldsetType
+export var Wysiwyg = WysiwygType
+export var File = FileType
+export var Link = LinkType
+export var FormBuilder = FormBuilderType
+export var Map = MapType
+export var LoginForm = LoginFormType
