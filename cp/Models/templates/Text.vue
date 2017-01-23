f1d89e25c8666f32d3d0acde71568a4c71b1c698
Federico <federico@p3in.com>
2017-01-23 07:36:00 -0500

    Final fixes on components export

--- cp/Models/templates/Text.vue
+++ cp/Models/templates/Text.vue
@@ -1,3 +1,11 @@
-<div @input="$emit('input', {value: $event.target.value, pointer: pointer})">
-    Text {{ pointer }}
-</div>
+<template lang="jade">
+div
+  textarea.textarea(@input="$emit('input', {value: $event.target.value, pointer: pointer})", :value="data")
+</template>
+
+<script>
+export default {
+  name: 'Formtext',
+  props: ['pointer', 'data']
+}
+</script>
