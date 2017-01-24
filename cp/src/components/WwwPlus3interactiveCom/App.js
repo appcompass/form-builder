dc8d5f0ed5c7fcbe2790950969c135519490e2a5
Federico <federico@p3in.com>
2017-01-24 07:40:48 -0500

    updates package.json, yarn.lock (do we keep it? i dont remember)

--- cp/src/components/WwwPlus3interactiveCom/App.js
+++ cp/src/components/WwwPlus3interactiveCom/App.js
@@ -1,14 +1,14 @@
+import HomePage from './Home'
 import SolutionsPage from './Solutions'
 import SolutionsOurProcessPage from './SolutionsOurProcess'
 import ProjectsPage from './Projects'
 import CompanyPage from './Company'
 import ContactPage from './Contact'
-import HomePage from './Home'
 import CustomerLoginPage from './CustomerLogin'
+export var Home = HomePage
 export var Solutions = SolutionsPage
 export var SolutionsOurProcess = SolutionsOurProcessPage
 export var Projects = ProjectsPage
 export var Company = CompanyPage
 export var Contact = ContactPage
-export var Home = HomePage
 export var CustomerLogin = CustomerLoginPage
