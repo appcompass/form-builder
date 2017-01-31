19c3db07a7c4aaf48ff14799c48e3acfa7969ae6
Federico <federico@p3in.com>
2017-01-31 10:51:45 -0500

    PageBuilder UI correctly addresses content, dynamic repeatables

--- websites/Controllers/PageContentController.php
+++ websites/Controllers/PageContentController.php
@@ -12,9 +12,22 @@ class PageContentController extends AbstractChildController
         $this->repo = $repo;
     }
 
+    public function index(Request $request, $parent)
+    {
+        return [
+            'data' => $parent->buildContentTree(true),
+            'view' => $this->repo->view
+        ];
+    }
+
     public function show($parent, $model)
     {
         $model->load('section.form');
+
+        return [
+            'form' => $model->section->form ? $model->section->form->render() : null,
+            'content' => $model->content
+        ];
 
         return $model->section->form->render();
     }
