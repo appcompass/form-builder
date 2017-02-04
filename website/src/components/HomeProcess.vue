6060d38c7a137f83a84b46675fab4e046992c2bf
Federico <federico@p3in.com>
2017-02-04 12:11:40 -0500

    Removes website

--- website/src/components/HomeProcess.vue
+++
@@ -1,9 +0,0 @@
-<template lang="jade">
-section.section-module.section-process
-  .row
-    .medium-8.columns.medium-centered
-      .section-header
-        h2.section-heading Our Process
-        .section-desc
-          p Explore our development process
-</template>
