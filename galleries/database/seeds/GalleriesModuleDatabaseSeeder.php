e5c50eb62101b1d818bc9b75d759b25046a77d97
Federico <federico@p3in.com>
2017-02-22 16:33:20 -0500

    :bug: Syntax Error

--- galleries/database/seeds/GalleriesModuleDatabaseSeeder.php
+++ galleries/database/seeds/GalleriesModuleDatabaseSeeder.php
@@ -29,7 +29,7 @@ class GalleriesModuleDatabaseSeeder extends Seeder
             $builder->string('Path', 'path')->list();
             $builder->file('Photo', 'photo')->list(false);
             $builder->string('Photo Name', 'title')->list()->validation(['required'])->sortable()->searchable();
-        })->linkToResources(['galleries.photos.index', 'galleries.photos.create', 'galleries.photo.edit'])
+        })->linkToResources(['galleries.photos.index', 'galleries.photos.create', 'galleries.photos.show'])
             ->getForm();
 
         // WebsiteBuilder::edit($cp->id)->linkForm($form);
