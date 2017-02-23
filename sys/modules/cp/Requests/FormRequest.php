383f529ded9409212c69996844ccfd07b1089936
Federico <federico@p3in.com>
2017-02-23 14:36:37 -0500

    Got basic automatic validation working. Form->rules() compiles the array of rules to feed into Validator. Cp\Requests\FormRequest form lookup via resource. To get working link resource.update resource to the form containing the validation rules.

---
+++ sys/modules/cp/Requests/FormRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace P3in\Requests;
+
+use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
+use \Illuminate\Validation\Validator;
+use P3in\Models\Form;
+use Route;
+
+class FormRequest extends BaseFormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        $form = Form::byResource(Route::current()->getName())->first();
+
+        if ($form) {
+
+            return $form->rules();
+
+        } else {
+
+            \Log::info('No validation found for resource ' . Route::current()->getName());
+
+            return [];
+
+        }
+
+    }
+
+    public function withValidator(Validator $validator)
+    {
+        return $validator->errors();
+
+        dd($validator->fails());
+
+        if ($validator->fails()) {
+
+            return ['errors' => $validator->errors()->all()];
+
+        }
+    }
+
+}
