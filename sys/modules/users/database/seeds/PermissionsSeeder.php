29c3d86cad0574680d0e5ac43a6def1f64998030
Federico <federico@p3in.com>
2017-02-26 08:59:41 -0500

    Linked validation to other Models

--- sys/modules/users/database/seeds/PermissionsSeeder.php
+++ sys/modules/users/database/seeds/PermissionsSeeder.php
@@ -16,43 +16,6 @@ class PermissionsSeeder extends Seeder
      */
     public function run()
     {
-        // \DB::statement('TRUNCATE permissions CASCADE');
-
-        // Permission::create(['name' => 'media']);
-        // Permission::create(['name' => 'deletions']);
-        // Permission::create(['name' => 'something']);
-
-        DB::statement("DELETE FROM forms WHERE name = 'permissions'");
-
-        FormBuilder::new('permissions', function (FormBuilder $builder) {
-            $builder->string('Name', 'label')
-                ->list()
-                ->validation(['required'])
-                ->sortable()
-                ->searchable();
-            $builder->text('Description', 'description')
-                ->list(false)
-                ->validation(['required'])
-                ->sortable()
-                ->searchable();
-            $builder->string('Created', 'created_at')
-                ->list()
-                ->edit(false)
-                ->validation(['required'])
-                ->sortable()
-                ->searchable();
-        })->linkToResources(['permissions.index', 'permissions.show', 'permissions.create']);
-
-        DB::statement("DELETE FROM forms WHERE name = 'user-permissions'");
-
-        FormBuilder::new('user-permissions', function (FormBuilder $builder) {
-            $builder->string('Name', 'label')
-                ->list()
-                ->validation(['required'])
-                ->sortable()
-                ->searchable();
-        })->linkToResources(['users.permissions.index']);
-
         Permission::create([
             'type' => 'logged-user',
             'label' => 'User',
--- sys/modules/users/database/seeds/UsersModuleDatabaseSeeder.php
+++ sys/modules/users/database/seeds/UsersModuleDatabaseSeeder.php
@@ -3,14 +3,15 @@
 namespace P3in\Seeders;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Seeder;
-use P3in\Builders\FormBuilder;
-use P3in\Models\Form;
-use P3in\Models\Permission;
-use P3in\Models\User;
 use P3in\Seeders\GroupsTableSeeder;
 use P3in\Seeders\PermissionsSeeder;
 use P3in\Seeders\UsersTableSeeder;
+use Illuminate\Database\Seeder;
+use P3in\Builders\FormBuilder;
+use P3in\Models\Permission;
+use P3in\Models\Form;
+use P3in\Models\User;
+use DB;
 
 class UsersModuleDatabaseSeeder extends Seeder
 {
@@ -24,16 +25,15 @@ class UsersModuleDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        \DB::statement('TRUNCATE users CASCADE');
-        \DB::statement('TRUNCATE permissions CASCADE');
-        \DB::statement('TRUNCATE groups CASCADE');
+        DB::statement('TRUNCATE users CASCADE');
+        DB::statement('TRUNCATE permissions CASCADE');
+        DB::statement('TRUNCATE groups CASCADE');
 
         $this->call(GroupsTableSeeder::class);
         $this->call(PermissionsSeeder::class);
         $this->call(UsersTableSeeder::class);
 
-        \DB::statement("DELETE FROM forms WHERE name = 'users'");
-
+        DB::statement("DELETE FROM forms WHERE name = 'users'");
         FormBuilder::new('users', function (FormBuilder $builder) {
             $builder->string('First Name', 'first_name')->list()->validation(['required'])->sortable()->searchable();
             $builder->string('Last Name', 'last_name')->list()->validation(['required'])->sortable()->searchable();
@@ -44,17 +44,25 @@ class UsersModuleDatabaseSeeder extends Seeder
             $builder->secret('Password', 'password')->validation(['required']);
         })->linkToResources(['users.index', 'users.show', 'users.create', 'users.update', 'users.store']);
 
-        //
-        //  GROUPS
-        //
-        \DB::statement("DELETE FROM forms WHERE name = 'groups'");
-
+        DB::statement("DELETE FROM forms WHERE name = 'groups'");
         FormBuilder::new('groups', function (FormBuilder $builder) {
             $builder->string('Group Name', 'name')->list()->validation(['required'])->sortable()->searchable();
             $builder->string('Group Label', 'label')->list()->validation(['required'])->sortable()->searchable();
             $builder->text('Description', 'description')->list(false)->validation(['required'])->sortable()->searchable();
             $builder->string('Date Added', 'created_at')->list()->edit(false)->sortable();
-        })->linkToResources(['groups.index', 'groups.show', 'groups.create', 'groups.update']);
+        })->linkToResources(['groups.index', 'groups.show', 'groups.store', 'groups.update']);
+
+        DB::statement("DELETE FROM forms WHERE name = 'user-permissions'");
+        FormBuilder::new('user-permissions', function (FormBuilder $builder) {
+            $builder->string('Name', 'label')->list()->validation(['required'])->sortable()->searchable();
+        })->linkToResources(['users.permissions.index']);
+
+        DB::statement("DELETE FROM forms WHERE name = 'permissions'");
+        FormBuilder::new('permissions', function (FormBuilder $builder) {
+            $builder->string('Name', 'label')->list()->validation(['required'])->sortable()->searchable();
+            $builder->text('Description', 'description')->list(false)->validation(['required'])->sortable()->searchable();
+            $builder->string('Created', 'created_at')->list()->edit(false)->validation(['required'])->sortable()->searchable();
+        })->linkToResources(['permissions.index', 'permissions.show', 'permissions.create', 'permissions.store', 'permissions.update']);
 
         Model::reguard();
     }
