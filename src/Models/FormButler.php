ab0b9adc03932e4743c551b69915951310e1b3ab
Federico Francescato <federico@p3in.com>
2017-03-21 16:06:32 -0400

    Extend FormRequest validation to custom named forms

--- src/Models/FormButler.php
+++ src/Models/FormButler.php
@@ -7,11 +7,25 @@ use P3in\Models\Form; // in case we move this
 
 class FormButler
 {
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $form_name  The form name
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
     public static function get($form_name)
     {
         return (new static)->resolveFormFromString($form_name)->render();
     }
 
+    /**
+     * { function_description }
+     *
+     * @param      \P3in\Requests\FormRequest  $request  The request
+     *
+     * @return     <type>                      ( description_of_the_return_value )
+     */
     private function parseRequest(FormRequest $request)
     {
         if ($request->has('form')) {
@@ -23,11 +37,26 @@ class FormButler
         return;
     }
 
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $form_name  The form name
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
     private function resolveFormFromString($form_name)
     {
         return Form::whereName($form_name)->firstOrFail();
     }
 
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $form_name  The form name
+     * @param      <type>  $content    The content
+     *
+     * @return     array   ( description_of_the_return_value )
+     */
     public static function store($form_name, $content)
     {
         // @TODO vvvv not really: what we wanna do here is resolve the form, get the linked Model
